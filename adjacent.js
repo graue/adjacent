140d80bc32698650f79bf39a741d2bd932c6f081
Scott Feeney <scott@oceanbase.org>
2014-07-05 23:10:10 -0400

    Perform solve steps using cell neighbors

--- adjacent.js
+++ adjacent.js
@@ -55,7 +55,6 @@ exports.parseGame = function(str) {
     throw new Error('invalid adjacent board');
   }
 
-
   var elements = matches[2].split(',').slice(0, -1);
 
   if(elements.length !== (boardSize * boardSize)){
@@ -93,6 +92,77 @@ function printBoard(board) {
       console.log(str[1]);
     }
   }
+}
+
+// Returns the universe of possible cell values in a board of the given size.
+function universe(size) {
+  return _u.range(1, size+1);
+}
+
+// Return a list of the numbers adjacent to num, in a board of size size.
+function adjacencies(num, size) {
+  return _u.intersection([num-1, num+1], universe(size));
+}
+
+// Return a list of the numbers neither adjacent nor equal to num, in a
+// board of size size.
+function nonAdjacencies(num, size) {
+  return _u.difference(universe(size), [num-1, num, num+1]);
+}
+
+var relCoords = [
+  [-1,  0, 'U'],
+  [ 1,  0, 'D'],
+  [ 0, -1, 'L'],
+  [ 0,  1, 'R']];
+
+// Eliminate possibilities at the given cell based on possibilities of
+// adjacent cells.
+function eliminateAt(board, row, col) {
+  var size = board.length;
+  var options = board[row][col].num;
+  if (options.length === 1) return options;
+  var adjs = board[row][col].adj;
+
+  return _u.intersection.apply(null, relCoords.map(function(coords) {
+    return [coords[0]+row, coords[1]+col, coords[2]];
+  }).filter(function(coords) {
+    return coords[0] >= 0 && coords[0] < size &&
+      coords[1] >= 0 && coords[1] < size;
+  }).map(function(coords) {
+    var selector;
+    var neighborOptions = board[coords[0]][coords[1]].num;
+    if (adjs.indexOf(coords[2]) !== -1) {
+      selector = adjacencies;
+    } else {
+      selector = nonAdjacencies;
+    }
+    return _u.union.apply(null, neighborOptions.map(function(val) {
+      return selector(val, size);
+    }));
+  }));
+}
+
+// Iterates over board cells trying to make inferences that reduce the
+// possibilities at each cell (modifying 'board' in-place). Returns true
+// if anything was changed.
+function solveStep(board) {
+  var changed = false;
+
+  for (var row = 0; row < board.length; row++) {
+    for (var col = 0; col < board.length; col++) {
+      var newPossibilities = eliminateAt(board, row, col);
+      if (newPossibilities.length === 0) {
+        throw new Error('board is unsolvable: no options at ' + row + ', ' +
+                        col);
+      }
+      if (newPossibilities.length < board[row][col].num.length) {
+        board[row][col].num = newPossibilities;
+        changed = true;
+      }
+    }
+  }
+  return changed;
 }
 
 printBoard(exports.parseGame(sampleGame));
